fd69f06d0038201774cc1ccb7b57d560ec04bb4e
mayaekie <mayaekie071@ed.amdsb.ca>
2025-01-16 01:11:40 -0500

    Add project files.

---
+++ Space Racer/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Space_Racer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.playerOne = new System.Windows.Forms.PictureBox();
+            this.moveTimer = new System.Windows.Forms.Timer(this.components);
+            this.playerTwo = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.playerOne)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.playerTwo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // playerOne
+            // 
+            this.playerOne.Image = ((System.Drawing.Image)(resources.GetObject("playerOne.Image")));
+            this.playerOne.Location = new System.Drawing.Point(141, 311);
+            this.playerOne.Name = "playerOne";
+            this.playerOne.Size = new System.Drawing.Size(50, 50);
+            this.playerOne.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.playerOne.TabIndex = 0;
+            this.playerOne.TabStop = false;
+            // 
+            // moveTimer
+            // 
+            this.moveTimer.Enabled = true;
+            this.moveTimer.Interval = 20;
+            this.moveTimer.Tick += new System.EventHandler(this.moveTimerEvent);
+            // 
+            // playerTwo
+            // 
+            this.playerTwo.Image = ((System.Drawing.Image)(resources.GetObject("playerTwo.Image")));
+            this.playerTwo.Location = new System.Drawing.Point(289, 311);
+            this.playerTwo.Name = "playerTwo";
+            this.playerTwo.Size = new System.Drawing.Size(50, 50);
+            this.playerTwo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.playerTwo.TabIndex = 1;
+            this.playerTwo.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 373);
+            this.Controls.Add(this.playerTwo);
+            this.Controls.Add(this.playerOne);
+            this.Name = "Form1";
+            this.Text = "Space Race";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.playerOne)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.playerTwo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox playerOne;
+        private System.Windows.Forms.Timer moveTimer;
+        private System.Windows.Forms.PictureBox playerTwo;
+    }
+}
+
---
+++ Space Racer/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Space_Racer
+{
+    public partial class Form1 : Form
+    {
+        //global variables
+        bool moveUp, moveDown, wUp, sDown;
+
+        int speed = 12;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void moveTimerEvent(object sender, EventArgs e)
+        {
+            if(moveUp == true && playerOne.Top > 0)
+            {
+                playerOne.Top -= speed;
+            }
+
+            if (moveDown == true && playerOne.Top < 250)
+            {
+                playerOne.Top += speed;
+            }
+
+            if (wUp == true && playerTwo.Top > 0)
+            {
+                playerTwo.Top -= speed;
+            }
+
+            if (sDown == true && playerTwo.Top < 250)
+            {
+                playerTwo.Top += speed;
+            }
+        }
+
+        private void keyIsDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Down)
+            {
+                moveDown = true;
+            }
+
+            if (e.KeyCode == Keys.Up)
+            {
+                moveUp = true;
+            }
+
+            if (e.KeyCode == Keys.W)
+            {
+                wUp = true;
+            }
+
+            if (e.KeyCode == Keys.S)
+            {
+                sDown = true;
+            }
+        }
+
+        private void keyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Down)
+            {
+                moveDown = false;
+            }
+
+            if (e.KeyCode == Keys.Up)
+            {
+                moveUp = false;
+            }
+
+            if (e.KeyCode == Keys.W)
+            {
+                wUp = false;
+            }
+
+            if (e.KeyCode == Keys.S)
+            {
+                sDown = false;
+            }
+        }
+    }
+}
